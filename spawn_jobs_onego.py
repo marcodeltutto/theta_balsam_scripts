0b54347a1dc59e54eebe3cc412b17d02dc4c4dca
Marco Del Tutto <marco.deltutto@gmail.com>
2021-07-22 20:08:57 +0000

    Update

--- spawn_jobs_onego.py
+++ spawn_jobs_onego.py
@@ -1,29 +1,41 @@
-from balsam.api import Job, App, site_config
-from time import time
+# Spawn Balsam jobs for production
+# All stages (gen, g4, detsim, ...) are run in a single job
 
-n_submissions = 2       # Number of submissions (== number of prodsingles)
-n_events = 5            # Number of events per submission
+from time import time
+import argparse
+from balsam.api import Job, App, site_config
+
+parser = argparse.ArgumentParser(description='Spawn jobs.')
+parser.add_argument('-S', '-s', '--sbnd', action='store_true')
+parser.add_argument('-I', '-i', '--icarus', action='store_true')
+args = parser.parse_args()
+
+n_submissions = 64*8       # Number of submissions (== number of prodsingles)
+n_events = 20            # Number of events per submission
 num_nodes = 1            # Number of compute nodes required (> 1 implies MPI usage)
 node_packing_count = 64  # Maximum number of concurrent runs per node.
 extra_tag = '1_node_sbnd_test'
 fhicl_dir = "/lus/grand/projects/neutrino_osc_ADSP/production/theta_balsam_scripts/fcls/"
 
-detector = "sbnd"
-software = "sbndcode"
-version = "v09_26_00"
-qual = "e20:prof"
-prod_fcl = "run_mpvmpr_sbnd.fcl"
+if args.sbnd:
+    detector = "sbnd"
+    software = "sbndcode"
+    version = "v09_26_00"
+    qual = "e20:prof"
+    prod_fcl = "run_mpvmpr_sbnd.fcl"
 
-# detector = "icarus"
-# software = "icaruscode"
-# version = "v09_26_00"
-# qual = "e20:prof"
-# prod_fcl = "run_mpvmpr_icarus.fcl"
+if args.icarus:
+    detector = "icarus"
+    software = "icaruscode"
+    version = "v09_26_00"
+    qual = "e20:prof"
+    prod_fcl = "run_mpvmpr_icarus.fcl"
+
 
 timestamp = int(time())
 
 for j in range(n_submissions):
-    print('Spawning submission:', j)
+    if j % 10 == 0: print('Spawning submission:', j)
     #
     # ProdMultiPart
     #
