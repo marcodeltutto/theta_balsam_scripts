ca0bf58394e8b014bdbb6d870a69f436f6bfa551
Corey Adams <corey.adams@anl.gov>
2021-09-21 13:54:25 +0000

    Add cosmic tagger generation apps and spawning script

--- apps/cosmic_tagger.py
+++ apps/cosmic_tagger.py
@@ -1,6 +1,11 @@
 from pathlib import Path
-from balsam.site import ApplicationDefinition
-from balsam.api import site_config
+# from balsam.site import ApplicationDefinition
+from balsam.api import site_config, ApplicationDefinition
+
+import os
+from typing import Dict
+import jinja2
+import h5py
 
 # echo $'#{{fcl_name}}\nphysics.bs.s: "3"' > wrapped_{{{{fcl_name}}}}
 # lar -c prodsingle_mu_bnblike.fcl --nevts {{nevts}} --output {{output}}
@@ -9,20 +14,35 @@ class CosmicTagger(ApplicationDefinition):
     """
     Generate files for CosmicTagger
     """
+    site = "cosmic-tagger-regen"
     environment_variables = {}
     command_template = '''
 echo "Job starting!"
 date
 hostname
-singularity run -B /grand/projects/:/grand/projects/:rw /lus/grand/projects/neutrino_osc_ADSP/containers/fnal-wn-sl7.sing <<EOF
+echo "Current directory: "
+pwd
+
+singularity run -B /lus/grand/projects/:/lus/grand/projects/:rw /lus/grand/projects/neutrino_osc_ADSP/containers/fnal-wn-sl7.sing <<EOF
+
+    echo "Running in: "
+    pwd
+    echo ""
     #setup SBNDCODE:
     source /lus/grand/projects/neutrino_osc_ADSP/software/larsoft/products/setup
-    # setup sbndcode v09_24_02 -q e20:prof
     setup {{software}} {{version}} -q {{qual}}
     # get the fcls
     cp -r {{fhicl_dir}}/*.fcl .
 
     lar -c {{fhicl}} --nevts {{nevts}} --output {{output}}
+
+    # Set up gallery framework
+    setup hdf5 v1_10_5a -q e20
+    setup cmake v3_18_2
+    source /lus/grand/projects/datascience/cadams/gallery-framework/config/setup.sh
+
+    python /lus/grand/projects/datascience/cadams/gallery-framework/UserDev/SuperaLight/supera_sbnd.py --file {{output}}
+
 EOF
 echo "Job finishing!"
 date
@@ -33,11 +53,33 @@ hostname
 '''
     parameters = {}
     transfers = {}
+    cleanup_files = ["*.root", "*.db", "*core", ".fcl"]
 
     def preprocess(self):
         self.job.state = "PREPROCESSED"
 
     def postprocess(self):
+        # several cleanup actions:
+        # Check the output file exists:
+        output_file = self.job.get_parameters()['output']
+        if not os.path.exists(output_file):
+           self.job.state = "FAILED"
+           self.job.state_data = {"reason" : f"Output file {output_file} does not exist"}
+        output_file = output_file.replace(".root","_larcv.h5")
+        if not h5py.is_hdf5(output_file):
+           self.job.state = "FAILED"
+           print("HDF5 file does not exist!")
+           self.job.state_data = {"reason": f"H5Py.is_file returned false for output_file: {output_file}"}
+           return
+
+        # Lastly, check that the files have the right number of events.
+        with h5py.File(output_file) as h:
+            if len(h["Events/event_id"]) == 0:
+                print("Length of HDF5 file is 0, ERROR")
+                self.job.state_data = {"reason" : "hdf5 file is length 0"}
+                self.job.state = "FAILED"
+
+
         self.job.state = "POSTPROCESSED"
 
     def shell_preamble(self):
@@ -49,33 +91,19 @@ hostname
     def handle_error(self):
         self.job.state = "FAILED"
 
-
+CosmicTagger.sync()
 
 # --source-list infiles.txt
 class Merge(ApplicationDefinition):
     """
     Application that merges files
     """
+    site = "cosmic-tagger-regen"
     environment_variables = {}
     command_template = '''
-echo "Job starting!"
-date
-hostname
-singularity run -B /lus/grand/projects/neutrino_osc_ADSP:/lus/grand/projects/neutrino_osc_ADSP:rw /lus/grand/projects/neutrino_osc_ADSP/containers/fnal-wn-sl7.sing <<EOF
-    #setup SBNDCODE:
-    source /lus/grand/projects/neutrino_osc_ADSP/software/larsoft/products/setup
-    # setup sbndcode v09_24_02 -q e20:prof
-    setup {{software}} {{version}} -q {{qual}}
-    touch empty.fcl
-    lar -c empty.fcl --nevts {{nevts}} --nskip {{nskip}} --output {{output}} --source p*.root
-EOF
-echo "Job finishing!"
-date
-echo "\nJob finished in: $(pwd)"
-echo "Available files:"
-ls
-hostname
-'''
+    source /lus/grand/projects/datascience/cadams/cosmic_tagger_gen_env/bin/activate
+    merge_larcv3_files.py -ol {{output_file}} -il {{input_file_list}}
+    '''
     parameters = {}
     transfers = {}
 
@@ -86,7 +114,7 @@ hostname
         for parent in self.job.parent_query():
             # Get the parent's workdir
             parent_workdir = site_config.data_path.joinpath(parent.workdir)  # This is a Path object
-            parent_files = parent_workdir.glob("p*.root")
+            parent_files = parent_workdir.glob("c*.h5")
             for i, parent_root_file in enumerate(parent_files):
                 if not Path(parent_root_file).is_symlink():
                     # Only symlink newly created files (not files that were previously symlinked)
@@ -96,6 +124,14 @@ hostname
         # print('End Running PREPROCESS for Merge')
 
         self.job.state = "PREPROCESSED"
+
+    def _render_command(self, arg_dict: Dict[str, str]) -> str:
+        diff = set(self.parameters.keys()).difference(arg_dict.keys())
+        if diff:
+            raise ValueError(f"Missing required args: {diff} (only got: {arg_dict})")
+
+        # sanitized_args = {key: shlex.quote(str(arg_dict[key])) for key in self.parameters}
+        return jinja2.Template(self.command_template).render(arg_dict)
 
     def postprocess(self):
         self.job.state = "POSTPROCESSED"
@@ -108,3 +144,58 @@ hostname
 
     def handle_error(self):
         self.job.state = "FAILED"
+
+Merge.sync()
+
+class Preprocess(ApplicationDefinition):
+    """
+    Application that merges files
+    """
+    site = "cosmic-tagger-regen"
+    environment_variables = {}
+    command_template = '''
+    source /lus/grand/projects/datascience/cadams/cosmic_tagger_gen_env/bin/activate
+    run_processor.py -c {{config}} -il {{input}} -ol {{output}}
+    '''
+    parameters = {}
+    transfers = {}
+
+    def preprocess(self):
+        # print('Running PREPROCESS for Preprocess')
+        # Get the parent
+
+        for parent in self.job.parent_query():
+            # Get the parent's workdir
+            parent_workdir = site_config.data_path.joinpath(parent.workdir)  # This is a Path object
+            parent_files = parent_workdir.glob("c*.h5")
+            for i, parent_root_file in enumerate(parent_files):
+                if not Path(parent_root_file).is_symlink():
+                    # Only symlink newly created files (not files that were previously symlinked)
+                    Path(parent_root_file.name).symlink_to(parent_root_file)
+                    # txt_file.write(parent_root_file.name + '\n')
+        # txt_file.close()
+        # print('End Running PREPROCESS for Merge')
+
+        self.job.state = "PREPROCESSED"
+
+    def _render_command(self, arg_dict: Dict[str, str]) -> str:
+        diff = set(self.parameters.keys()).difference(arg_dict.keys())
+        if diff:
+            raise ValueError(f"Missing required args: {diff} (only got: {arg_dict})")
+
+        # sanitized_args = {key: shlex.quote(str(arg_dict[key])) for key in self.parameters}
+        return jinja2.Template(self.command_template).render(arg_dict)
+
+    def postprocess(self):
+        self.job.state = "POSTPROCESSED"
+
+    def shell_preamble(self):
+        pass
+
+    def handle_timeout(self):
+        self.job.state = "RESTART_READY"
+
+    def handle_error(self):
+        self.job.state = "FAILED"
+
+Preprocess.sync()
--- spawn_ct_jobs.py
+++ spawn_ct_jobs.py
@@ -3,15 +3,15 @@
 
 from time import time
 import argparse
-from balsam.api import Job, App, Site
+from balsam.api import Job, Site
 
 
-n_submissions = 64*2     # Number of submissions (== number of prodsingles)
-n_events = 20            # Number of events per submission
+n_submissions = 32*1     # Number of submissions (== number of prodsingles)
+n_events = 5             # Number of events per submission
 num_nodes = 1            # Number of compute nodes required (> 1 implies MPI usage)
-node_packing_count = 64  # Maximum number of concurrent runs per node.
-extra_tag = '1_node_sbnd_test'
-fhicl_dir = "/grand/projects/datascience/cadams/theta_balsam_scripts/fcls/cosmic_tagger/"
+node_packing_count = 48  # Maximum number of concurrent runs per node.
+extra_tag = f'{num_nodes}_node_merge_test_{node_packing_count}npc'
+fhicl_dir = "/lus/grand/projects/datascience/cadams/theta_balsam_scripts/fcls/cosmic_tagger/"
 fhicl_base = "prod_cosmictagger_sample_{sample}.fcl"
 
 
@@ -21,42 +21,241 @@ version = "v09_27_00_02"
 qual = "e20:prof"
 # prod_fcl = "run_mpvmpr_sbnd.fcl"
 
-# FIgure out the site:
-site = Site.objects.filter(path="/grand/projects/datascience/cadams/cosmic-tagger-regen")[0]
-print(site)
+# Figure out the site:
+site = Site.objects.filter(path="/lus/grand/projects/datascience/cadams/cosmic-tagger-regen")[0]
 
 timestamp = int(time())
-ct_app = App.objects.get(site_id=site.id, class_path="cosmic_tagger.CosmicTagger")
-print(ct_app)
+ct_app = "CosmicTagger"
+merge_app = "Merge"
+preprocess_app = "Preprocess"
 
-for j in range(n_submissions):
-    if j % 10 == 0: print('Spawning submission:', j)
+# Here, we do the following for train/val/test
+# Each larsoft job generates 5 events with cosmic overlay.
+#   - It converts that to larcv as well
+# There is a larsoft job for all 3 flavors
+# Then, a merge job puts the 3 files together into one larcv file.
+# Then, a preprocess job does the larcv preprocessing ops
+# Then, another merge job puts 15 event files together.
+# In total, we target statistics of:
+# - 500,000 events in train
+# - 50,000 events in val
+# - 100,000 events in test
+# - 100,000 events in beam-only
 
+# For train/val/test:
+# - Generate 5 events per file, 3 samples (WORKGROUP), 10x each, merged into 150 events. (SUBSAMPLE)
+# - Preprocess each file, merge 25 preprocessed files into a merged file (3750 events) (PREPROC)
+# - Requires 134 workflows in train (5e5 events) (WORKFLOW)
+# - Requires 13 workflows in val (5e4 events)
+# - Requires 26 workflows in test
+
+# For beam events:
+# - generate 5 events at a time, merge 20 together (100 events)
+# - 25 jobs together get preprocessed and merged (2500 events)
+# - Requires 40 workflows for beam
+
+# Organization:
+# - inside the data directory, store things in train/test/val/beam at top level.
+# - then, index by workflow (0 to 133 for train, for example)
+# - inside there, index by preprocessed file (25)
+# - inside there, index by flavor (nueCC, NC, numuCC)
+# - inside there, index by subsample (0 to 9)
+# - Example workdir is data/train/27/2/nueCC/9
+
+
+# Computational requirements:
+# - First jobs of 5 events take ~30 minutes per core
+# - Memory limits to 48 jobs per node.
+# - Subsequent jobs *may* run faster.
+# - 500000 events * 0.5 hours / 48 jobs per node = 5208 node hours
+# - Would require approximately 128 nodes for 40 hours.
+# - better estimate needed.
+
+def spawn_larsoft_workgroup(workflow : str, name : str, subsample_index : int, preproc_index : int, events_per_file = 5):
+    '''
+    Create a group of 3 files (nueCC, numuCC, NC) and return the jobs.
+    '''
     sample_jobs = []
-
+    larcv_files = []
 
     # Spawn a nueCC, numuCC, NC job each time:
     for sample in ["NC", "nueCC", "numuCC"]:
+        out_name = f"cosmic_tagger_{sample}_{subsample_index}.root"
         job = Job(
-            workdir=f"prod_cosmictagger/{sample}/{j}/",
-            app_id=ct_app.id,
-            tags={"sample":sample, 'n_submissions': n_submissions, 'n_events': n_events, 'extra': extra_tag},
-            parameters={
-                "software": software,
-                "version": version,
-                "qual": qual,
-                "fhicl_dir": fhicl_dir,
-                "fhicl": fhicl_base.format(sample=sample),
-                "nevts": f"{n_events}",
-                "output": f"cosmic_tagger_{sample}.root"},
-            node_packing_count=node_packing_count,
-            num_nodes=num_nodes,
-            parent_ids=(),
+            workdir    = f"{name}/{sample}/{subsample_index}",
+            app_id     = ct_app,
+            tags       = {
+                "workflow"  : workflow,
+                "sample"    : sample,
+                "n_events"  : events_per_file,
+                "subsample" : subsample_index,
+                "preproc"   : preproc_index},
+            parameters = {
+                "software"  : software,
+                "version"   : version,
+                "qual"      : qual,
+                "fhicl_dir" : fhicl_dir,
+                "fhicl"     : fhicl_base.format(sample=sample),
+                "nevts"     : f"{events_per_file}",
+                "output"    : out_name,
+                },
+            node_packing_count = node_packing_count,
+            num_nodes = num_nodes,
+            parent_ids = (),
         )
-        job.save()
         sample_jobs.append(job)
+        larcv_files.append(out_name.replace(".root","_larcv.h5"))
 
-    # Here would be a merge job for the larcv output:
+    return sample_jobs, larcv_files
+
+def spawn_larsoft_subsample(workflow : str, name : str, n_workgroups : int, preproc_index : int):
+
+    all_jobs         = []
+    all_output_files = []
+    events_per_file  = 5
+
+    for i in range(n_workgroups):
+
+        jobs, larcv_files = spawn_larsoft_workgroup(
+            workflow        = workflow,
+            name            = name,
+            subsample_index = i,
+            preproc_index   = preproc_index,
+            events_per_file = events_per_file)
+
+
+        all_jobs += jobs
+        all_output_files += larcv_files
+
+    total_events = events_per_file * 3 * n_workgroups
+
+
+    # save all the jobs:
+    all_jobs = Job.objects.bulk_create(all_jobs)
+
+    # get the parent IDs:
+    parent_ids = [s.id for s in all_jobs]
+
+
+    # create a merge job for these sub samples:
+    merge_job = Job(
+        workdir = f"{name}/merged/",
+        app_id =merge_app,
+        tags        = {
+            "workflow"      : workflow,
+            "sample"        : "merge",
+            "n_events"      : total_events,
+            "preproc"       : preproc_index,
+        },
+        parameters  = {
+            "output_file"   : f"cosmic_tagger_merged_{preproc_index}.h5",
+            "input_file_list" : " ".join(all_output_files),
+        },
+        node_packing_count = node_packing_count,
+        parent_ids=parent_ids
+    )
+    merge_job.save()
+
+
+    # And, create a preprocessing job:
+    preproc_job = Job(
+        workdir = f"{name}/preprocessed/",
+        app_id  = preprocess_app,
+        tags    = {
+            "workflow"      : workflow,
+            "sample"        : "preproc",
+            "n_events"      : total_events,
+            "preproc"       : preproc_index,
+        },
+        parameters = {
+            "config" : "/lus/grand/projects/datascience/cadams/theta_balsam_scripts/preprocess.json",
+            "input"  : f"cosmic_tagger_merged_{preproc_index}.h5",
+            "output" : f"cosmic_tagger_merged_{preproc_index}_processed.h5"
+        },
+        node_packing_count = node_packing_count,
+        parent_ids = [merge_job.id]
+    )
+    preproc_job.save()
+
+    # Need to return the job to merge the next steps together
+    return preproc_job
+
+def spawn_workflow(name : str, n_workgroups : int, n_sub_workflows, workflow_index : int):
+
+    # We spawn subsamples, collect the preprocess jobs, and merge the outputs together.
+    preproc_jobs = []
+    for i in range(n_sub_workflows):
+        top_dir = f"{name}/{workflow_index}/{i}/"
+
+        job = spawn_larsoft_subsample(name, top_dir, n_workgroups=n_workgroups, preproc_index=i)
+
+        preproc_jobs.append(job)
+
+
+    parent_ids = [s.id for s in preproc_jobs]
+
+    total_events = 0
+    for j in preproc_jobs: total_events += int(j.tags["n_events"])
+
+    output_files = []
+    for j in preproc_jobs: output_files.append(j.get_parameters()["output"])
+
+    # create a merge job for these sub samples:
+    merge_job = Job(
+        workdir = f"{name}/merged/",
+        app_id  = merge_app,
+        tags        = {
+            "workflow"      : name,
+            "sample"        : "merge",
+            "n_events"      : total_events,
+        },
+        parameters  = {
+            "output_file"   : f"cosmic_tagger_merged_{workflow_index}.h5",
+            "input_file_list" : " ".join(output_files),
+        },
+        node_packing_count = node_packing_count,
+        parent_ids=parent_ids
+    )
+    merge_job.save()
+
+    pass
+
+# Testing configuration: just a few small pieces:
+#
+# name="test"
+# for i_workflow in range(26):
+#     print(f"{name} workflow {i_workflow}")
+#     spawn_workflow("test", n_workgroups=10, n_sub_workflows=25, workflow_index=i_workflow)
+
+name="val"
+for i_workflow in range(13):
+    print(f"{name} workflow {i_workflow}")
+    spawn_workflow("val", n_workgroups=10, n_sub_workflows=25, workflow_index=i_workflow)
+
+# spawn_workflow("train_demo", n_workgroups=3, n_sub_workflows=4, workflow_index=1)
+
+
+    #
+    # # We add the beam simulation job too:
+    # job = Job(
+    #     workdir=f"prod_cosmictagger/{extra_tag}/{j}/base/",
+    #     app_id=ct_app.id,
+    #     tags={"sample":"base", 'n_submissions': n_submissions, 'n_events': n_events, 'extra': extra_tag},
+    #     parameters={
+    #         "software": software,
+    #         "version": version,
+    #         "qual": qual,
+    #         "fhicl_dir": fhicl_dir,
+    #         "fhicl": fhicl_base.format(sample="base"),
+    #         "nevts": f"{n_events}",
+    #         "output": out_name
+    #         },
+    #     node_packing_count=node_packing_count,
+    #     num_nodes=num_nodes,
+    #     parent_ids=(),
+    # )
+    # job.save()
+
 
 
     # for i in range(n_events):
